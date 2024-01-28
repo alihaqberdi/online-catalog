ee2caedb985d6414edfda5bf29b69f6d9f98783d
alihaqberdi <alhppypro@gmail.com>
2024-01-28 23:44:22 +0500

    feat: big changes

--- common/views.py
+++ common/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 
 from product.models import Category, Collection, Product, TopCategory
@@ -9,12 +10,27 @@ def main_page(request):
     if len(all_collection_product) > 12:
         all_collection_product = all_collection_product[:12]
     category = TopCategory.objects.first()
+    all_category = Category.objects.filter(is_active=True).prefetch_related("product_set")
+    default_category = all_category.first()
+
+    items_per_page = 12
+    paginator = Paginator(default_category.product_set.all(), items_per_page)
+    page = request.GET.get("page", 1)
+
+    try:
+        obj_list = paginator.page(page)
+    except PageNotAnInteger:
+        obj_list = paginator.page(1)
+    except EmptyPage:
+        obj_list = paginator.page(paginator.num_pages)
 
     context = {
         "collection": collection,
         "all_collection_product": all_collection_product,
         "category": category.category.all()[1:] if category else None,
         "default_category": category.category.first() if category else None,
+        "all_category": all_category,
+        "obj_list": obj_list,
     }
     return render(request, "index.html", context)
 
